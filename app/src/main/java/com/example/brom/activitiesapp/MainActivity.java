94deb2b2607f9a6499d585bb38e38c9c6e1cb9ac
MechaKnightz <mechaknightz@hotmail.com>
2019-05-20 11:28:32 +0200

    added mountaindetails view and list item view layout

--- app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
+++ app/src/main/java/com/example/brom/activitiesapp/MainActivity.java
@@ -2,17 +2,40 @@ package com.example.brom.activitiesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
     private int[] mountainHeights ={4478,4808,6190};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
+    private ArrayList<String> listData = new ArrayList<>(Arrays.asList(mountainNames));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //created the adapter with the layout elements defined earlier
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item_textview, R.id.my_item_textview, listData);
+        //get the listview and set the adapter
+        ListView myListView = findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+        //listener event for clicking on a list item
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+                String out = "Name: " + mountainNames[pos] + ", Location: " + mountainLocations[pos]
+                        + ", Height: " + mountainHeights[pos] + "m.";
+
+            }
+        });
 
         // 1. Create a ListView as in previous assignment
         // 2. Create a new activity named "MountainDetailsActivity
